88332f712e505b35b6fc585bbce63ac5a9f7d311
AlexandreMercador <amercador@gmail.com>
2022-06-29 17:19:39 -0300

    Criação de domain para futuros clientes

---
+++ src/main/java/com/api/helpr/domain/ClienteFuturo.java
@@ -0,0 +1,88 @@
+package com.api.helpr.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class ClienteFuturo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String telefone;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataCriacao = LocalDate.now();
+
+	public ClienteFuturo() {
+		super();
+	}
+
+	public ClienteFuturo(Integer id, String nome, String email, String telefone, LocalDate dataCriacao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
